4adaf6ab2d4d6c9dae1264f24c0eb704446cba78
Talha Khalid <117529116+khalidtalha00@users.noreply.github.com>
2026-04-29 23:51:43 +0530

    added vercel config file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,15 @@
-const API_KEY =import.meta.env.VITE_TMDB_API_KEY
-const BASE_URL = "https://api.themoviedb.org/3"; 
+const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const response = await fetch(`${TMDB_BASE_URL}/movie/popular?api_key=${API_KEY}`);
   const data = await response.json();
   return data.results;
 };
 
 export const searchMovies = async (query) => {
   const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+    `${TMDB_BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
       query
     )}`
   );
